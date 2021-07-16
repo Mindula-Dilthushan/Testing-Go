f38e31c5581c58ee46b7568a2ce22c130efc3644
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-07-16 20:45:41 +0530

    Hello Go Print

---
+++ 01_Hello_Go/helloGo.go
@@ -0,0 +1,14 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//Testing-Go
+//module-01
+//21-07-16
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Go Programming")
+}
